6b7475fb98052d86b430ebb06d7274c528fd7c5a
Biranchi Narayan Padhi <34164657+biranchinarayanpadhi@users.noreply.github.com>
2017-12-02 20:58:40 +0530

    Add files via upload

---
+++ particle/README.txt
@@ -0,0 +1,2 @@
+This is a small html page where  i have implemented the particle.js and typed.js
+for an independence day.Every source API is present in this.
---
+++ particle/js.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded',function(){
+Typed.new('.element',{
+strings: ["Happy Independence day","Maa Tujhe Salam","kalia"],
+typespeed:0
+});
+
+});
+
+
+ /* var typed = new Typed('.element', {
+    stringsElement: '#typed-strings'
+  });
+*/
